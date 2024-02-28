69be68fc775a77e46593189935fd1bf41b2ffcec
vinegrette <korshunovzhe@yandex.ru>
2024-02-28 12:59:36 +0300

    1

--- 3.py
+++ 3.py
@@ -1,6 +1,6 @@
-def f(x):
-    if x%4==0 and x%100!=0 and x%400!=0:
-        return print('Год',x,'- високосный')
+def f(n):
+    if n%4==0 and n%100!=0 and n%400!=0:
+        return print('Год',n,'- високосный')
     else:
        return print('Этот год не високосный')
 
