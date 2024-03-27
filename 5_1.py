444d3225f56c10ff013fa74859661cfbfcb2b173
vinegrette <korshunovzhe@yandex.ru>
2024-03-27 12:33:37 +0300

    1

---
+++ 5_1.py
@@ -0,0 +1,11 @@
+lst=[1,2,3,4,5]
+x=int(input('Введите число'))
+
+if x in lst:
+    print(lst)
+    print(
+    print('Вы угадали число!')
+else:
+    print(lst)
+    print(x)
+    print('Вы не угадали нихуя')
