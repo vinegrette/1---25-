dbe1e6767b61e0166604d46e47eca04f5582c680
vinegrette <korshunovzhe@yandex.ru>
2024-04-24 12:33:05 +0300

    1

--- 7_2.py
+++ 7_2.py
@@ -4,5 +4,7 @@ from PIL import Image
 i=Image.open(r'C:\1-MD-25-Korshunov\g.jpg')
 
 ri=i.resize((320,200))
-ri
-ri.save('g_2.jpg')
+riz= ri.transpose(Image.FLIP_LEFT_RIGHT)
+riz.save('gz.jpg')
+riz1= ri.transpose(Image.FLIP_LEFT_RIGHT)
+riz1.save('gz_2.jpg')
